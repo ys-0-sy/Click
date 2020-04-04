0dae94625c8134e28118abeda48837e08efad2ca
Yuta Saito <yuta.saito0703@gmail.com>
2020-04-04 21:37:34 +0900

    add settings.swift file

---
+++ Memolation/Pages/Settings.swift
@@ -0,0 +1,33 @@
+//
+//  Settings.swift
+//  Memolation
+//
+//  Created by saito on 2020/04/04.
+//  Copyright © 2020 ys-0-sy. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Settings()
+    }
+}
+
+struct About: View {
+  var body: some View {
+    Text("about")
+  }
+}
+
+struct Settings: View {
+  var body: some View {
+    Text("about")
+  }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
