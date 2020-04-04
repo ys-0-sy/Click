facc175bc2e60dcd3bbe68be2626d5265cf79cd7
Yuta Saito <yuta.saito0703@gmail.com>
2020-04-04 21:37:17 +0900

    add nav tabview

--- Memolation/Pages/NavTabView.swift
+++ Memolation/Pages/NavTabView.swift
@@ -7,11 +7,108 @@
 //
 
 import SwiftUI
+import Combine
 
 struct NavTabView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    NavTab()
+  }
+}
+
+struct NavTab: View {
+  @ObservedObject var navViewRouter = NavViewRouter()
+  @State var showPopUp = false
+
+  var body: some View {
+    GeometryReader { geometry in
+      VStack {
+        Spacer()
+        if self.navViewRouter.currentView == "home" {
+          Text("Home")
+        } else if self.navViewRouter.currentView == "settings" {
+          Text("Settings")
+        }
+        Spacer()
+        ZStack {
+          if self.showPopUp {
+             PlusMenu()
+               .offset(y: -geometry.size.height/6)
+           }
+          HStack {
+            Image("translation")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .padding(8)
+              .frame(width: geometry.size.width/3, height: 75)
+              .foregroundColor(self.navViewRouter.currentView == "home" ? .black : .gray)
+              .onTapGesture {
+                self.navViewRouter.currentView = "home"
+            }
+            ZStack {
+          
+              Circle()
+                .foregroundColor(Color.white)
+                .frame(width: 75, height: 75)
+              Image(systemName: "plus.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 75, height: 75)
+                .foregroundColor(.blue)
+                .rotationEffect(Angle(degrees: self.showPopUp ? 90: 0))
+            }
+              .offset(y: -geometry.size.height/10/2)
+              .onTapGesture {
+                withAnimation {
+                  self.showPopUp.toggle()
+                }
+              }
+            Image(systemName: "gear")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .padding(20)
+              .frame(width: geometry.size.width/3, height: 75)
+              .foregroundColor(self.navViewRouter.currentView == "settings" ? .black : .gray)
+              .onTapGesture {
+                self.navViewRouter.currentView = "settings"
+            }
+
+          }
+          .frame(width: geometry.size.width, height: geometry.size.height/10)
+          .background(Color.white.shadow(radius: 2))
+        }
+      }.edgesIgnoringSafeArea(.bottom)
     }
+  }
+}
+
+struct PlusMenu: View {
+  var body: some View {
+    HStack(spacing: 50) {
+      ZStack {
+        Circle()
+          .foregroundColor(Color.blue)
+          .frame(width: 70, height: 70)
+        Image(systemName: "camera")
+        .resizable()
+          .aspectRatio(contentMode: .fit)
+        .padding(20)
+          .frame(width: 70, height: 70)
+          .foregroundColor(.white)
+      }
+      ZStack {
+        Circle()
+          .foregroundColor(Color.blue)
+          .frame(width: 70, height: 70)
+        Image(systemName: "photo")
+        .resizable()
+          .aspectRatio(contentMode: .fit)
+        .padding(20)
+          .frame(width: 70, height: 70)
+          .foregroundColor(.white)
+      }
+    }
+    .transition(.scale)
+  }
 }
 
 struct NavTabView_Previews: PreviewProvider {
---
+++ Memolation/Pages/NavViewRouter.swift
@@ -0,0 +1,16 @@
+//
+//  NavViewRouter.swift
+//  Memolation
+//
+//  Created by saito on 2020/04/04.
+//  Copyright © 2020 ys-0-sy. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class NavViewRouter: ObservableObject {
+    @Published var currentView = "home"
+
+}
