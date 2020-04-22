e0a615a481c737b3f1c60fef11e99fe70f70e4dd
Yuta Saito <yuta.saito0703@gmail.com>
2020-04-23 00:42:22 +0900

    added Keys struct

---
+++ Click/Model/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  Cl!ck
+//
+//  Created by saito on 2020/04/23.
+//  Copyright © 2020 ys-0-sy. All rights reserved.
+//
+
+import Foundation
+
+struct Keys: Codable {
+    var googleAPIKey: String
+}
